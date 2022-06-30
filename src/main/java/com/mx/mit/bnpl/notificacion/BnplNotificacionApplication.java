5e63f23ae7769296489a0bd1fd9da668cdd37dd1
ccanibe <c.canibe@gmail.com>
2022-06-30 11:17:36 -0500

    Cambio de estructura en secrets

--- src/main/java/com/mx/mit/bnpl/notificacion/BnplNotificacionApplication.java
+++ src/main/java/com/mx/mit/bnpl/notificacion/BnplNotificacionApplication.java
@@ -3,11 +3,15 @@ package com.mx.mit.bnpl.notificacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mx.mit.bnpl.notificacion.config.PropertiesListener;
+
 @SpringBootApplication
 public class BnplNotificacionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BnplNotificacionApplication.class, args);
+		SpringApplication app = new SpringApplication(BnplNotificacionApplication.class);
+		app.addListeners(new PropertiesListener());
+		app.run(args);
 	}
 
 }
---
+++ src/main/java/com/mx/mit/bnpl/notificacion/secrets/MitIdentitySecrets.java
@@ -0,0 +1,51 @@
+package com.mx.mit.bnpl.notificacion.secrets;
+
+import java.util.Base64;
+
+import org.json.JSONObject;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.InvalidParameterException;
+import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MitIdentitySecrets{
+		public JSONObject get() {
+		JSONObject result;
+		String secretName = "mit-bnpl-dev-identity";
+		String AWS_Region = "us-east-1";
+		AWSSecretsManager client = AWSSecretsManagerClientBuilder.standard().withRegion(AWS_Region).build();
+
+		String decodedBinarySecret;
+		GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest().withSecretId(secretName);
+		GetSecretValueResult getSecretValueResult   = null;
+
+		try {
+			getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+		}
+		catch (ResourceNotFoundException e) {
+			log.error("The requested secret " + secretName + " was not found");
+		}
+		catch (InvalidRequestException e) {
+			log.error("The request was invalid due to: " + e.getMessage());
+		}
+		catch (InvalidParameterException e) {
+			log.error("The request had invalid params: " + e.getMessage());
+		}
+		
+		if (getSecretValueResult.getSecretString() != null) {
+			result = new JSONObject(getSecretValueResult.getSecretString());
+		} else {
+			decodedBinarySecret = String.valueOf( Base64.getDecoder().decode(getSecretValueResult.getSecretBinary()).array() );
+			result = new JSONObject(decodedBinarySecret);
+		}
+
+		return result;
+
+	}
+}
